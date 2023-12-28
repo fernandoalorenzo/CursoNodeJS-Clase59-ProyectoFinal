160e77f242e06d06f5be507ce9fdfe81bf43035d
Fernando Lorenzo <fernando.a.lorenzo@gmail.com>
2023-12-28 20:44:26 -0300

    2023-12-28

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /* eslint-disable no-unused-vars */
 import React from "react";
 
@@ -14,20 +13,3 @@ const Footer = () => {
 };
 
 export default Footer;
-=======
-/* eslint-disable no-unused-vars */
-import React from "react";
-
-const Footer = () => {
-	return (
-		<footer className="text-center bg-primary bg-gradient align-items-center col align-self-center">
-			<p>
-				Copyright ©{new Date().getFullYear()}. All rights reserved.
-				Powered by Fernando Lorenzo
-			</p>
-		</footer>
-	);
-};
-
-export default Footer;
->>>>>>> 9c4dde004aef6f7a655dac25a95a2ae1922ff1b5
